b0444e5dc4f3def55a5d701f3ec032d96f33cde1
andreskeb01 <13470210@itcampeche.edu.mx>
2019-02-19 11:44:34 -0600

    agregado boton de registro en el welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,87 +1,28 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.auth-layout')
 
-        <title>Servicios ITES</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
+@section('content')
         <br>
         <br>
         <br>
-        <div class="full-height">
+        <div class="full-height justify-content-center text-center">
             <div class="content">
-                @if(session()->has('unautenticated'))
-                    <div style="color: #6f000f; font-size: 15px; font-weight: 600; ">{{session('unautenticated')}}</div>
-                @endif
-                <h1>Servicios del Instituto de Estudios Superiores <br> Rene Descartes</h1>
 
-                <br>
-                <br>
-                <div class="links">
-                    <a href="{{ route('login_index')  }}">Servicios Bibliotecarios</a>
-                    <a href="{{ route('login_computo_index') }}">Servicios Centro de Cómputo</a>
+                <h1>Servicios del Instituto de Estudios Superiores <br> Rene Descartes</h1>
+                <br><br><br>
+                <div class="links " >
+                    <a href="{{ route('login_index')  }}" class="pr-5">Servicios Bibliotecarios</a>
+                    <a href="{{ route('login_computo_index') }}" class="pl-5" >Servicios Centro de Cómputo</a>
+                </div>
+                    <br><br>
+                <div>
+                    <input type ='button' class="btn btn-primary py-2 px-5"  value = 'Registrarme' onclick="location.href = 'register'"/>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+        <br><br><br><br><br><br><br>
+@endsection
+@section('footer')
+    <div class="container">
+        <p class="m-0 text-center text-white">&copy; 2018 <strong>Instituto René Descartes.</strong> Todos los derechos reservados</p>
+    </div>
+    @endsection
